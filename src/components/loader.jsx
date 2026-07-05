c4d91d0935ccc7e9f61fe2febc6c047f754bf16b
Abdallha0 <abdallhamo7@gmail.com>
2026-07-05 13:16:43 +0300

    feat: update dependencies and enhance Loader component; refactor CSS for improved design system

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -2,13 +2,21 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col gap-y-2 items-center justify-center min-h-screen">
       <div className="relative flex items-center justify-center">
         <div className="w-16  h-16 rounded-full bg-linear-to-tr from-blue-600 via-sky-400 to-indigo-500 animate-spin blur-[2px]">
           <div className="size-full rounded-full bg-slate-900 m-0.75 w-[calc(100%-6px)] h-[calc(100%-6px)]"></div>
         </div>
         <div className="w-10 absolute h-10 rounded-full border-4 border-double border-sky-400 animate-spin">
           <div className="size-full rounded-full bg-slate-900 m-0.75 w-[calc(100%-6px)] h-[calc(100%-6px)]  "></div>
+        </div>
+      </div>
+      <div className="flex font-semibold ml-6 gap-1">
+        LOADING
+        <div className="flex space-x-1 mt-4 justify-center items-center bg-white dark:invert">
+          <div className="h-1.25 w-1.25 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+          <div className="h-1.25 w-1.25 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+          <div className="h-1.25 w-1.25 bg-black rounded-full animate-bounce"></div>
         </div>
       </div>
     </div>
