45bd01e0ab190a9619fd3943c6a59e18e8dd5be0
mazin189 <mazen.ahmed.abdelattii@gmail.com>
2026-07-15 17:54:21 +0300

    adding components

--- src/pages/EditProductPage.jsx
+++ src/pages/EditProductPage.jsx
@@ -1,9 +1,14 @@
 import EditProductsHeader from "../components/products/EditProductsHeader";
+import EditProductImgUploader from "../components/products/EditProductImgUploader";
+import EditProductDataSec from "../components/products/EditProductDataSec";
 
 function EditProductPage() {
 return(
   <div className="p-6">
     <EditProductsHeader/>
+    <EditProductImgUploader />
+    <EditProductDataSec />
+
   </div>
 )
 
