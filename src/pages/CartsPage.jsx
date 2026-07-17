86acaaee2a3a7890474ca1b0e5a6df5c1c1b8bb9
Mariam-1-m <mariumaabdelrahman7@gmail.com>
2026-07-17 20:01:51 +0300

    Some fixes

--- src/pages/CartsPage.jsx
+++ src/pages/CartsPage.jsx
@@ -2,7 +2,7 @@ import CartSec1 from '../components/cart/sec1'
 import CartContainer from '../components/cart/CartContainer';
 function CartsPage(){
     return(
-<div className='flex flex-col  items-center justify-center p-3'>
+<div className='flex flex-col  items-center justify-center px-3 py-5'>
     <CartSec1/>
     <CartContainer/>
 </div>
